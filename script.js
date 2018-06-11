b6e3ef3009ca4e15995b9060706e7ba076ce7054
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-11 13:55:39 +0500

    js inside html

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 //for css
 app.use(express.static('public'));
