636ed64f5ed25cb680e64ac8e464894a96b46ee6
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-11 11:02:00 +0500

    sending and receiving data

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 //for css
 app.use(express.static('public'));
 //for html
@@ -7,10 +10,16 @@ app.set('view engine', 'ejs')
 
 
 
-
+//displaying html requested page
 app.get('/', function (req, res) {
   //res.send('Hello World!');
   res.render('index');
+})
+
+//displaying form response
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
 })
 
 app.listen(3000, function () {
